cf09294a5b1adea6c410a758fccb1d53fa0635df
Alexey <ailyinchick@gmail.com>
2023-11-02 09:41:52 +0300

    First commit

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import pack01.Message;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < Message.values().length; i++) {
+			System.out.println(Message.values()[i]);
+		}
+		
+	}
+
+}
---
+++ src/pack01/Message.java
@@ -0,0 +1,9 @@
+package pack01;
+
+public enum Message {
+	WARNING,
+	ERROR,
+	EXCEPTION,
+	NOTOFICATION,
+	DEFAULT
+}
