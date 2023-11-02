17229a0a3e94f9b494d8d12172552397339a121b
Alexey <ailyinchick@gmail.com>
2023-11-02 11:52:36 +0300

    added some feature to merge into main

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,11 @@ import pack01.Message;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Hello from Main");
 		for (int i = 0; i < 12; i++) {
 			System.out.println(Message.getNext());
 		}
+		// Theese lines was added in local branch to
 	}
 
 }
