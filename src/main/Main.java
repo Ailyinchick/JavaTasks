646ab281b4f05e6e28348d539fdee1b3f7d9cc07
Alexey <ailyinchick@gmail.com>
2023-11-22 16:31:10 +0300

    Created base structure for future sort algorithms

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,33 +2,44 @@ package main;
 
 import java.util.Random;
 
-import pack01.Sorter;
+import Enteties.*;
+import Enums.Names;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int[] arr = new int[20];
+		Entity[] arr = new Entity[10];
+		Random rand = new Random();
+		Sorter<Entity> sorter = new Sorter<>();
 		
-		fillIntArr(arr);
-		System.out.println("Before sorting: ");;
-		printIntArr(arr);
-		
-		Sorter.insertionSort(arr);
-		System.out.println("After sorting: ");
-		printIntArr(arr);
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = new Entity(rand.nextInt(10, 40), Names.getNext());
+		}
+
+		printAsList(arr);
+		sorter.sort(arr);
+		System.out.println("*\nAfter sorting:\n*");
+		printAsList(arr);
 	}
 	
-	public static void printIntArr(int[] arr) {
+	
+	public static void fillIntArr(int[] arr) {
+		Random rand = new Random();
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = rand.nextInt(100);
+		}
+	}
+	
+	public static<T> void printAsLine(T[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
 		System.out.println();
 	}
 	
-	public static void fillIntArr(int[] arr) {
-		Random rand = new Random();
+	public static<T> void printAsList(T[] arr) {
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = rand.nextInt(100);
+			System.out.println(arr[i]);
 		}
 	}
 
