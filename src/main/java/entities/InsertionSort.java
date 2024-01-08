84fd8260599106cc910c7fd771b2d36a07be456e
= <ailyinchick@gmail.com>
2024-01-08 11:04:58 +0300

    Generalized methods in InsertionSort

--- src/main/java/entities/InsertionSort.java
+++ src/main/java/entities/InsertionSort.java
@@ -8,34 +8,26 @@ public class InsertionSort<T extends MyComparable<T>> extends SortStrategy<T> {
 
     @Override
     public void sortAsc(List<T> list) {
-        if (list == null || list.size() < 2) {
-            return;
-        }
-        for (int i = 1; i < list.size(); i += 1) {
-            T key = list.get(i);
-            int j = i - 1;
-            while (j >= 0 && (list.get(j).compareTo(key) == -1)) {
-                list.set(j+1, list.get(j));
-                j -= 1;
-            }
-            list.set(j+1, key);
-        }
+        sortWithOrder(list, -1);
     }
 
     @Override
     public void sortDesc(List<T> list) {
+        sortWithOrder(list, 1);
+    }
+
+    private void sortWithOrder(List<T> list, final int order) {
         if (list == null || list.size() < 2) {
             return;
         }
         for (int i = 1; i < list.size(); i += 1) {
             T key = list.get(i);
             int j = i - 1;
-            while (j >= 0 && (list.get(j).compareTo(key) == 1)) {
-                list.set(j+1, list.get(j));
+            while (j >= 0 && (list.get(j).compareTo(key) == order)) {
+                list.set(j + 1, list.get(j));
                 j -= 1;
             }
-            list.set(j+1, key);
+            list.set(j + 1, key);
         }
-
     }
 }
